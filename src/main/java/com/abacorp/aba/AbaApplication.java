423b98240bbd7a002c6602eca7853a1ca1f144d5
lminggvick <lminggvick@naver.com>
2020-06-25 15:49:11 +0900

    Add: Security config 기본 설정 추가 / Set DataSource

--- src/main/java/com/abacorp/aba/AbaApplication.java
+++ src/main/java/com/abacorp/aba/AbaApplication.java
@@ -2,8 +2,10 @@ package com.abacorp.aba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("/db.properties")
 public class AbaApplication {
 
     public static void main(String[] args) {
