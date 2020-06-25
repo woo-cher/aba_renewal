5200e0bb7a4eb34784a71e82e9881dcd091a098e
lminggvick <lminggvick@naver.com>
2020-06-25 11:12:11 +0900

    none: First initialize

---
+++ src/main/java/com/abacorp/aba/AbaApplication.java
@@ -0,0 +1,13 @@
+package com.abacorp.aba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbaApplication.class, args);
+    }
+
+}
