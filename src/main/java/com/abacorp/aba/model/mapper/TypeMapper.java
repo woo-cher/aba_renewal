22419e7445d260e338e736fb7d5f0ebcf8c59611
lminggvick <lminggvick@naver.com>
2020-07-08 09:47:30 +0900

    Add: OfferType 추가, 타입 매퍼 리팩토링 - #14

--- src/main/java/com/abacorp/aba/model/mapper/TypeMapper.java
+++ src/main/java/com/abacorp/aba/model/mapper/TypeMapper.java
@@ -2,5 +2,5 @@ package com.abacorp.aba.model.mapper;
 
 public interface TypeMapper {
     String getCode();
-    String getName();
+    String getValue();
 }
--- src/main/java/com/abacorp/aba/model/type/OfferType.java
+++ src/main/java/com/abacorp/aba/model/type/OfferType.java
@@ -4,19 +4,26 @@ import com.abacorp.aba.model.mapper.TypeMapper;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
-public enum UserRoleType implements TypeMapper {
+public enum OfferType implements TypeMapper {
 
     /**
      * 유저 권한
      */
-    ADMIN("ADMIN"),
-    USER("USER"),
+    ONE_ROOM("원룸"),
+    TWO_THREE_ROOM("투룸"),
+    EFFICIENCY_APT("오피스텔"),
+    APT("아파트"),
+    SHOP("상가"),
+    HOUSING("주택"),
+    OFFICE("사무실"),
+    LAND("사무실"),
+    BUILDING("건물"),
 
-    ANONYMOUS("ANONYMOUS");
+    UNKNOWN("UNKNOWN");
 
     private String value;
 
-    UserRoleType(String value) {
+    OfferType(String value) {
         this.value = value;
     }
 
@@ -32,7 +39,7 @@ public enum UserRoleType implements TypeMapper {
 
     @Override
     public String toString() {
-        return "PropertyType {" +
+        return "OfferType {" +
                 "value='" + value + '\'' +
                 '}';
     }
